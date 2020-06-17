de25e9c216f9ea18957416d28b78d5a6d7bdb421
Ulises Olivares <uolivares@gdl.cinvestav.mx>
2020-06-17 01:03:31 -0500

    Latest code

---
+++ src/cmake-build-release/CMakeFiles/optimization.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/bellford.cpp" "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/cmake-build-release/CMakeFiles/optimization.dir/bellford.cpp.o"
+  "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/dijkstra.cpp" "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/cmake-build-release/CMakeFiles/optimization.dir/dijkstra.cpp.o"
+  "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/distance.cpp" "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/cmake-build-release/CMakeFiles/optimization.dir/distance.cpp.o"
+  "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/exploracion.cpp" "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/cmake-build-release/CMakeFiles/optimization.dir/exploracion.cpp.o"
+  "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/main.cpp" "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/cmake-build-release/CMakeFiles/optimization.dir/main.cpp.o"
+  "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/raster.cpp" "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/cmake-build-release/CMakeFiles/optimization.dir/raster.cpp.o"
+  "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/tree.cpp" "/Users/ulisesolivares/Documents/GitHub/PowerPlantOptimization/src/cmake-build-release/CMakeFiles/optimization.dir/tree.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/Cellar/opencv/4.3.0_4/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
